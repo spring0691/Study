8665854d3564e8c7e36a014b3527ccd3db07af8e
yeram <ye069182@gmail.com>
2021-12-06 15:42:41 +0900

    12/06

---
+++ keras/keras26_ModelCheckPoint2_load.py
@@ -0,0 +1,57 @@
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
+
+#1 데이터 정제작업 !!
+datasets = load_boston()
+x = datasets.data
+y = datasets.target
+
+
+x_train,x_test,y_train,y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
+
+# #2. 모델링 
+# model = Sequential()
+# model.add(Dense(40, input_dim=13))
+# model.add(Dense(30))
+# model.add(Dense(20))
+# model.add(Dense(10))
+# model.add(Dense(1))
+
+
+#3. 컴파일, 훈련
+#model.compile(loss='mse', optimizer='adam') 
+
+# es = EarlyStopping(monitor='val_loss', patience=10, mode='min', verbose=1, restore_best_weights=True)
+# mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, save_best_only=True, filepath='./_ModelCheckPoint/keras26_2_MCP.hdf5')
+
+# hist = model.fit(x_train,y_train,epochs=50, batch_size=8,validation_split=0.25, callbacks=[es,mcp]) 
+
+# print("-------------------------------------------")
+# print(hist.history['val_loss']) # hist.history var_loss키 값의 value들을 출력해준다.
+# print("-------------------------------------------")
+
+
+#model.save("./_save/keras26_2_save_MCP.h5")
+# restore_best_weights True 했을때
+#model = load_model('./_ModelCheckPoint/keras26_2_MCP.hdf5')
+#model = load_model('./_save/keras26_2_save_MCP.h5')
+# 확인해봤는데 값이 똑같이 나온다.
+# restore_best_weights False 했을때
+#model = load_model('./_ModelCheckPoint/keras26_1_MCP.hdf5')
+model = load_model('./_save/keras26_1_save_MCP.h5')
+#4. 평가 , 예측
+loss = model.evaluate(x_test,y_test)
+print('loss : ', loss)
+
+y_predict = model.predict(x_test)
+
+r2 = r2_score(y_test,y_predict) 
+print('r2스코어 : ', r2)
+
+#loss :  45.410118103027344
+#r2스코어 :  0.456705915478698
