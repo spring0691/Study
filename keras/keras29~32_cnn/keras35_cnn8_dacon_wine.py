47f2dbf4f4be1ef8563ca475978f301cff503f22
yeram <ye069182@gmail.com>
2021-12-10 10:29:11 +0900

    12/09

--- keras/keras29~32_cnn/keras35_cnn8_dacon_wine.py
+++ keras/keras29~32_cnn/keras35_cnn8_dacon_wine.py
@@ -9,57 +9,42 @@ from sklearn.preprocessing import LabelEncoder
 from pandas import get_dummies
 import time
 
-from tensorflow.python.keras.regularizers import get
 
 #1. 데이터 로드 및 정제
 path = "../_data/dacon/wine/"   
 
-train = pd.read_csv(path + 'train.csv')                 
+train = pd.read_csv(path + 'train.csv') #train.csv를 train에 담아 사용해서 원본은 그대로있다. 
 test_file = pd.read_csv(path + 'test.csv')                  
 submit_file = pd.read_csv(path + 'sample_Submission.csv')     
 
-x = train.drop(['id','quality'], axis=1)   
-test_file = test_file.drop(['id'], axis=1) 
-y = train['quality']    
+x = train.drop(['id','quality'], axis=1)   # shape(3231, 12)
+test_file = test_file.drop(['id'], axis=1) # shape(3231, 12)
+y = train['quality']                       # shape(3231,)
+#print(y.value_counts())     # 6 5 7 8 4개의 value가 각각 1418 1069 539 108 97순으로 있는 다중분류모델
+ 
+# Le = LabelEncoder() + label = x.type + Le.fit(label) + x.type = Le.transform(label)
+x.type = LabelEncoder().fit_transform(x.type)                   # type의 white와 red값이 0,1로 바뀌어있음
+test_file.type = LabelEncoder().fit_transform(test_file.type)   # 상동
 
+#y = get_dummies(y)  # 원한인코딩 해준후 확인.(3231,5)
 
-#print(x.shape)  #(3231, 12)
-#print(y.shape)  #(3231, 5)
+x['quality'] = y         # x의 데이터셋에 y값을 price라는 이름의 칼럼으로 추가한다. 
 
+#print(x)              #pandas형 데이터라 index와 colmuns의 이름이 나옴. quality열이 추가되어 있는 것 확인.
 
-Le = LabelEncoder()     
-
-label = x['type']  
-Le.fit(label)       
-x['type'] = Le.transform(label)    
-
-
-label2 = test_file['type']
-Le.fit(label2)
-test_file['type'] =Le.transform(label2) 
-
-
-# numpy pandas로 변환후 pandas의 제공기능인 index정보와 columns정보를 확인할수있다.
-# 원래 판다스라서 그냥한다.
-
-#print(x)               #index와 colmuns의 이름이 나옴.
-
-#print(x.corr())        # 칼럼들의 서로서로의 상관관게를 수치로 확인할 수 있다.    절대값클수록 양 or 음의 상관관계 0에 가까울수록 서로 영향 없음
-
-x['y~~'] = y         # xx의 데이터셋에 y값을 price라는 이름의 칼럼으로 추가한다. 원본데이터는 그대로있다.    열 추가하는 방법.
-
-#print(x)              # price열이 추가되어 있는 것 확인.
-
-# print(x.corr())      # price와 어떤 열이 제일 상관관계가 적은지 확인.
+#print(x.corr())      # price와 어떤 열이 제일 상관관계가 적은지 확인.   .corr() -> 컬럼들의 상관관계를 수치로 보여주는 함수
 
 #########################################################
-# import matplotlib.pyplot as plt
-# import seaborn as sns   # 조금 더 이쁘게 만들게 도와줌.
-# plt.figure(figsize=(10,10))
-# sns.heatmap(data=x.corr(), square=True, annot=True, cbar=True)
-# # seaborn heatmap 개념정리
-# plt.show()
+import matplotlib.pyplot as plt
+import seaborn as sns   # 조금 더 이쁘게 만들게 도와줌.
+plt.figure(figsize=(10,10))
+sns.heatmap(data=x.corr(), square=True, annot=True, cbar=True)
+# seaborn heatmap 개념정리
+plt.show()
 ###########################################################
+# sulphates 0.027 pH 0.036 total sulfur dioxide 0.044 free sulfur dioxide 0.068 residual sugar 0.045 
+# citric acid 0.067 fixed acidity 0.082
+'''
 x = x.drop(['pH','sulphates','y~~'], axis=1)  
 test_file = test_file.drop(['pH','sulphates'],axis=1)
 
@@ -75,6 +60,8 @@ test_file = scaler.transform(test_file).reshape(len(test_file),5,2,1)
 
 y_train = get_dummies(y_train)
 y_test = get_dummies(y_test)
+
+
 #2. 모델링
 
 input1 = Input(shape=(5,2,1))
@@ -120,9 +107,9 @@ submit_file['quality'] = results
 
 acc= str(round(loss[1], 4)).replace(".", "_")
 submit_file.to_csv(path+f"result/accuracy_{acc}.csv", index = False)
+'''
 
-
-'''                                                                                   
+'''                                                                              
 결과정리                  일반레이어                      relu                   
                                                                                     
 안하고 한 결과 
