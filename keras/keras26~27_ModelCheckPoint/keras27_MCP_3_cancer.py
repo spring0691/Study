4e1ff8bfb558bd46cfdfed2444302cb93652a20c
yeram <ye069182@gmail.com>
2021-12-13 01:57:45 +0900

    12/12

--- keras/keras26~27_ModelCheckPoint/keras27_MCP_3_cancer.py
+++ keras/keras26~27_ModelCheckPoint/keras27_MCP_3_cancer.py
@@ -1,19 +1,20 @@
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.layers import Dense, Input
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler,StandardScaler,RobustScaler,MaxAbsScaler 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.metrics import r2_score
 import numpy as np
 import time
+from tensorflow.keras.utils import to_categorical 
 
 #1.데이터 로드 및 정제
 
-datasets = load_boston()
+datasets = load_breast_cancer()
 x = datasets.data
 y = datasets.target
-
+y = to_categorical(y)
 
 x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.9, shuffle=True, random_state=49)    
 
@@ -29,18 +30,18 @@ x_test = scaler.transform(x_test)
 
 #2. 모델구성,모델링
 
-input1 = Input(shape=(13,))
+input1 = Input(shape=(30,))
 dense1 = Dense(50)(input1)
 dense2 = Dense(30)(dense1)
 dense3 = Dense(15,activation="relu")(dense2)
 dense4 = Dense(8,activation="relu")(dense3)
 dense5 = Dense(5)(dense4)
-output1 = Dense(1)(dense5)
+output1 = Dense(2, activation='sigmoid')(dense5)
 model = Model(inputs=input1,outputs=output1)
 
 
 #3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam') 
+model.compile(loss='binary_crossentropy', optimizer='adam') 
 
 ti = time.time()
 kr = time.localtime(ti)
