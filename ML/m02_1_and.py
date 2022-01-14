030b3680927665879137f45e22a9bc587fdd4d5d
yeram <ye069182@gmail.com>
2022-01-14 11:03:16 +0900

    01/14

---
+++ ML/m02_1_and.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.svm import LinearSVC   #아주 예전모델  support vector machine
+from sklearn.metrics import accuracy_score
+
+#1. 데이터  AND 
+x_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+y_data = [0, 0, 0, 1]
+
+#2. 모델
+model = LinearSVC()
+
+#3. 훈련
+model.fit(x_data,y_data)
+
+#4. 평가, 예측
+y_predict = model.predict(x_data)
+print(x_data, "의 예측결과 : ", y_predict)
+results = model.score(x_data,y_data)
+print("model.score : ", results)
+
+acc = accuracy_score(y_data, y_predict)
+print("accuracy_score : ", acc)
---
+++ ML/m02_3_xor.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.svm import LinearSVC   
+from sklearn.metrics import accuracy_score
+
+#1. 데이터  XOR
+x_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+y_data = [0, 1, 1, 0]
+
+#2. 모델
+model = LinearSVC()
+
+#3. 훈련
+model.fit(x_data,y_data)
+
+#4. 평가, 예측
+y_predict = model.predict(x_data)
+print(x_data, "의 예측결과 : ", y_predict)
+results = model.score(x_data,y_data)
+print("model.score : ", results)
+
+acc = accuracy_score(y_data, y_predict)
+print("accuracy_score : ", acc)
+
+# 우리눈엔 쉬워보이지만 이 간단한걸 못한다 
+# xor 연산이란 같으면 0 = False, 다르면 1 = True를 반환해준다.
