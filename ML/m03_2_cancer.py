70e85ad04949afa434656b4300147f8ed06033c0
yeram <ye069182@gmail.com>
2022-01-14 12:27:30 +0900

    01/14

--- ML/m03_2_cancer.py
+++ ML/m03_2_cancer.py
@@ -1,6 +1,8 @@
 from sklearn.linear_model import Perceptron
 from sklearn.svm import LinearSVC,SVC   
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression     
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier,KNeighborsRegressor
+from sklearn.linear_model import LinearRegression,LogisticRegression
+from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
+
+from sklearn.datasets import load_breast_cancer,load_wine,fetch_covtype,load_boston,load_diabetes
---
+++ ML/m03_4_fetch_covtype.py
@@ -0,0 +1,6 @@
+from sklearn.linear_model import Perceptron
+from sklearn.svm import LinearSVC,SVC   
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression     
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
