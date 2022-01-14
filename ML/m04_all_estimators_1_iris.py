28d4b76a2b8f60d8a8679a3a9934e84fa6deb712
yeram <ye069182@gmail.com>
2022-01-14 14:49:06 +0900

    01/14

--- ML/m04_all_estimators_1_iris.py
+++ ML/m04_all_estimators_1_iris.py
@@ -31,3 +31,46 @@ for (name, algorithm) in allAlgorithms:
     except:
         print(name,'에서 오류떴어~')
    
+'''
+AdaBoostClassifier 의 정답률 :  0.6333333333333333
+BaggingClassifier 의 정답률 :  0.9333333333333333
+BernoulliNB 의 정답률 :  0.4
+CalibratedClassifierCV 의 정답률 :  0.9666666666666667
+CategoricalNB 의 정답률 :  0.3333333333333333
+ClassifierChain 에서 오류떴어~
+ComplementNB 의 정답률 :  0.6666666666666666
+DecisionTreeClassifier 의 정답률 :  0.9666666666666667
+DummyClassifier 의 정답률 :  0.3
+ExtraTreeClassifier 의 정답률 :  0.9
+ExtraTreesClassifier 의 정답률 :  0.9666666666666667
+GaussianNB 의 정답률 :  0.9666666666666667
+GaussianProcessClassifier 의 정답률 :  0.9666666666666667
+GradientBoostingClassifier 의 정답률 :  0.9666666666666667
+HistGradientBoostingClassifier 의 정답률 :  0.8666666666666667
+KNeighborsClassifier 의 정답률 :  1.0
+LabelPropagation 의 정답률 :  0.9666666666666667
+LabelSpreading 의 정답률 :  0.9666666666666667
+LinearDiscriminantAnalysis 의 정답률 :  1.0
+LinearSVC 의 정답률 :  0.9666666666666667
+LogisticRegression 의 정답률 :  0.9666666666666667
+LogisticRegressionCV 의 정답률 :  1.0
+MLPClassifier 의 정답률 :  0.9666666666666667
+MultiOutputClassifier 에서 오류떴어~
+MultinomialNB 의 정답률 :  0.6333333333333333
+NearestCentroid 의 정답률 :  0.9666666666666667
+NuSVC 의 정답률 :  0.9666666666666667
+OneVsOneClassifier 에서 오류떴어~
+OneVsRestClassifier 에서 오류떴어~
+OutputCodeClassifier 에서 오류떴어~
+PassiveAggressiveClassifier 의 정답률 :  0.7
+Perceptron 의 정답률 :  0.9333333333333333
+QuadraticDiscriminantAnalysis 의 정답률 :  1.0
+RadiusNeighborsClassifier 의 정답률 :  0.4666666666666667
+RandomForestClassifier 의 정답률 :  0.9333333333333333
+RidgeClassifier 의 정답률 :  0.9333333333333333
+RidgeClassifierCV 의 정답률 :  0.8333333333333334
+SGDClassifier 의 정답률 :  0.8
+SVC 의 정답률 :  1.0
+StackingClassifier 에서 오류떴어~
+VotingClassifier 에서 오류떴어~
+'''
