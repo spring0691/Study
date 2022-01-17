e3d709231376abc9b6c12bbf64fdcead119186cf
yeram <ye069182@gmail.com>
2022-01-17 22:39:57 +0900

    01/17

--- ML/ML07_gridSearch/m07_gridSearchCV4_xgboost&LGBM.py
+++ ML/ML07_gridSearch/m07_gridSearchCV4_xgboost&LGBM.py
@@ -1,6 +1,7 @@
 import pandas as pd, os, numpy as np
 from sklearn.model_selection import train_test_split,KFold,GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import GradientBoostingRegressor,GradientBoostingClassifier
 from xgboost import XGBRegressor, XGBClassifier
 from lightgbm import LGBMRegressor, LGBMClassifier
 from catboost import CatBoostRegressor, CatBoostClassifier
@@ -24,11 +25,24 @@ x_test = scaler.transform(x_test)
 
 n_splits = 5
 kfold = KFold(n_splits=n_splits, shuffle=True, random_state=66)
-
+'''
 parameters = [{'n_estimators' : [100,200], 'max_depth' : [6, 8, 10, 12], 'min_samples_leaf' : [3, 5, 7, 10], 
                'min_samples_split' : [2, 3, 5, 10] }]
 
 model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, n_jobs=-1) 
+
+parameters = {'n_estimators':[100, 500],'learning_rate':[0.05,0.1]}
+model = GridSearchCV(GradientBoostingRegressor(), param_grid=parameters, cv=kfold, n_jobs=-1)
+'''
+parameters = {'max_depth' : [3],
+          'eta': [0.1],
+          'objective':['reg:linear'],
+          'eval_metric':['logloss'],
+          'early_stopping':[100]
+          }
+model = GridSearchCV(XGBRegressor(), parameters, cv=kfold, n_jobs=-1)
+#model = GridSearchCV(LGBMRegressor(), parameters, cv=kfold, n_jobs=-1)
+#model = GridSearchCV(CatBoostRegressor(),  cv=kfold, n_jobs=-1)
 
 model.fit(x_train,y_train)
 print('나는 회귀모델!')      
@@ -51,6 +65,13 @@ print('\n')
 model.score로 구한 값은요~ :  0.36442296420636966
 r2_score :  0.36442296420636966
 최적 튠 R2 :  0.36442296420636966
+
+나는 회귀모델!
+최적의 매개변수는요~ :  GradientBoostingRegressor(n_estimators=500)
+최적의 파라미터는요~ :  {'learning_rate': 0.1, 'n_estimators': 500}
+model.score로 구한 값은요~ :  0.3402718133230821
+r2_score :  0.3402718133230821
+최적 튠 R2 :  0.3402718133230821
 
 
 '''
