c6b582255317d2bfba475a5d5528688997f263cc
yeram <ye069182@gmail.com>
2022-01-18 10:48:31 +0900

    01/18

--- ML/ML07~08_Grid&RandomSearch/m07_gridSearchCV4_xgboost&LGBM.py
+++ ML/ML07~08_Grid&RandomSearch/m07_gridSearchCV4_xgboost&LGBM.py
@@ -1,5 +1,5 @@
 import pandas as pd, os, numpy as np
-from sklearn.model_selection import train_test_split,KFold,GridSearchCV
+from sklearn.model_selection import train_test_split,KFold,GridSearchCV,RandomizedSearchCV
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import GradientBoostingRegressor,GradientBoostingClassifier
 from xgboost import XGBRegressor, XGBClassifier
@@ -26,24 +26,19 @@ x_test = scaler.transform(x_test)
 n_splits = 5
 kfold = KFold(n_splits=n_splits, shuffle=True, random_state=66)
 '''
-parameters = [{'n_estimators' : [100,200], 'max_depth' : [6, 8, 10, 12], 'min_samples_leaf' : [3, 5, 7, 10], 
-               'min_samples_split' : [2, 3, 5, 10] }]
-
+parameters = [{'n_estimators' : [100,200], 'max_depth' : [6, 8, 10, 12], 'min_samples_leaf' : [3, 5, 7, 10], 'min_samples_split' : [2, 3, 5, 10] }]
 model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, n_jobs=-1) 
 
 parameters = {'n_estimators':[100, 500],'learning_rate':[0.05,0.1]}
 model = GridSearchCV(GradientBoostingRegressor(), param_grid=parameters, cv=kfold, n_jobs=-1)
 '''
-parameters = {'max_depth' : [3],
-          'eta': [0.1],
-          'objective':['reg:linear'],
-          'eval_metric':['logloss'],
-          'early_stopping':[100]
-          }
-model = GridSearchCV(XGBRegressor(), parameters, cv=kfold, n_jobs=-1)
+parameters = {'max_depth' : [3],'eta': [0.1],'objective':['reg:linear'],'eval_metric':['logloss'],'early_stopping':[100]}
+model = RandomizedSearchCV(XGBRegressor(), parameters, cv=kfold, n_jobs=-1, n_iter = 20, random_state=66)
 #model = GridSearchCV(LGBMRegressor(), parameters, cv=kfold, n_jobs=-1)
 #model = GridSearchCV(CatBoostRegressor(),  cv=kfold, n_jobs=-1)
 
+print(XGBRegressor().get_params().items())
+'''
 model.fit(x_train,y_train)
 print('나는 회귀모델!')      
 print("최적의 매개변수는요~ : ", model.best_estimator_)
@@ -56,15 +51,25 @@ print("r2_score : ", r2_score(y_test,y_pred))
 y_pred_best = model.best_estimator_.predict(x_test)                
 print("최적 튠 R2 : ", r2_score(y_test,y_pred_best))              
 print('\n')
+'''
+
+
+
+
+
+
+
+
+
 
 '''
 나는 회귀모델!
-최적의 매개변수는요~ :  RandomForestRegressor(max_depth=10, min_samples_leaf=3, min_samples_split=5,
-                      n_estimators=200)
+최적의 매개변수는요~ :  RandomForestRegressor(max_depth=10, min_samples_leaf=3, min_samples_split=5,n_estimators=200)
 최적의 파라미터는요~ :  {'max_depth': 10, 'min_samples_leaf': 3, 'min_samples_split': 5, 'n_estimators': 200}
 model.score로 구한 값은요~ :  0.36442296420636966
 r2_score :  0.36442296420636966
 최적 튠 R2 :  0.36442296420636966
+
 
 나는 회귀모델!
 최적의 매개변수는요~ :  GradientBoostingRegressor(n_estimators=500)
--- ML/ML07~08_Grid&RandomSearch/m09_HalvingGridSearch.py
+++ ML/ML07~08_Grid&RandomSearch/m09_HalvingGridSearch.py
@@ -54,4 +54,38 @@ y_pred_best = model.best_estimator_.predict(x_test)
 print("최적 튠 ACC : ", accuracy_score(y_test,y_pred_best))     # 각기 다른방식으로 값을 뽑아봄으로써 model에서 최고값을 주는지 평균값을 주는지 확인할 수 있다.
 print("걸린시간 : ", end - start)
 
+'''
+n_iterations: 2
+n_required_iterations: 5
+n_possible_iterations: 2
+min_resources_: 30
+max_resources_: 120
+aggressive_elimination: False
+factor: 3
+----------
+iter: 0
+n_candidates: 192
+n_resources: 30
+Fitting 5 folds for each of 192 candidates, totalling 960 fits
+----------
+iter: 1
+n_candidates: 64
+n_resources: 90
+Fitting 5 folds for each of 64 candidates, totalling 320 fits
+최적의 매개변수 :  SVC(C=1000, degree=6, gamma=0.001, kernel='sigmoid')
+최적의 파라미터 :  {'C': 1000, 'degree': 6, 'gamma': 0.001, 'kernel': 'sigmoid'}
+best_score_ :  0.9777777777777779
+model.score :  0.9666666666666667
+acc_score :  0.9666666666666667
+최적 튠 ACC :  0.9666666666666667
+걸린시간 :  1.7391059398651123
 
+Halving 모델은 기존의 GridSearch에서 모든걸 다 카운트하면서 세는 방식을 좀 보완한것인데 randomsearch가 dropout처럼 줄였다면 Halving은 2등분한다? 오히려 X2 한다?
+그러면 GridSearch보다 2배더 시간 걸리고 해야하는데 1번 돌때 상위 일정 %의 상위 값을 뽑고 2번째 돌때는 미리 뽑아놓은 값들로만 돌린다. 
+1번 돌릴때 전체를 일반 Grid처럼 싹 다 돌리는게 아니라 여기저기 조합해서 좀 뽑아써서 1번 돌릴때 일반 Grid의 100%까지는 다 안하고 그 이하로 돌린다. 그 후 위의 내용을 실행한다.
+HalvingGrid -> 데이터(params * cv)의 일부만 쓰겠다 일반Grid말고 HalvingGrid / Random -> 파라미터의 일부만 쓰겠다.   HalvingGrid와 Random은 살짝 다르다 그래서 
+
+중첩교차검증? 
+교차검증을 또 중첩하겠다 train & test로 일단 나눠서 그냥 실행 시킨후? train을 cv한다?
+
+'''
