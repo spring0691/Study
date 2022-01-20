5fe385923bfb87d41a5382158c1627b28da3a8dd
yeram <ye069182@gmail.com>
2022-01-20 12:45:40 +0900

    01/20

--- ML/ML10~13_pipeline&feature_importance.py/m13_feature_importances0_masterpackage.py
+++ ML/ML10~13_pipeline&feature_importance.py/m13_feature_importances0_masterpackage.py
@@ -2,16 +2,20 @@
 from sklearn.datasets import load_iris,load_breast_cancer,load_wine,fetch_covtype,load_boston,load_diabetes
 from sklearn.model_selection import train_test_split
 import warnings, numpy as np, pandas as pd,sys
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier     # tree계열에서만 feature_importances가 있다.
-from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,RandomForestRegressor,GradientBoostingRegressor
+from xgboost import XGBClassifier,XGBRegressor
 from sklearn.metrics import accuracy_score,r2_score,f1_score
 import matplotlib.pyplot as plt
+
 
 def plot_feature_importances_dataset(model):
     n_features = x.shape[1]
     plt.barh(np.arange(n_features),model.feature_importances_,align='center')
-    plt.yticks(np.arange(n_features), datasets.feature_names)
+    try:
+        plt.yticks(np.arange(n_features), datasets.feature_names)
+    except:
+        plt.yticks(np.arange(n_features), x.coulmns)
     plt.xlabel("Feature Importances")
     plt.ylabel("Features")
     plt.ylim(-1,n_features)
@@ -29,63 +33,77 @@ path = '../Project/Kaggle_Project/bike/'
 Bikedata = pd.read_csv(path + 'train.csv')                 
 dd =  {'Iirs':load_iris(),'Breast_cancer':load_breast_cancer(),'Wine':load_wine(),'Boston':load_boston(),'Diabets':load_diabetes(),'Bike':Bikedata,'Fetch_covtype':fetch_covtype()}
 
+
 #2. 모델링 설정
-model_list = [DecisionTreeClassifier(max_depth=5,random_state=66),RandomForestClassifier(max_depth=5,random_state=66),GradientBoostingClassifier(),XGBClassifier()]
+cla_model_list = [DecisionTreeClassifier(max_depth=5,random_state=66),RandomForestClassifier(max_depth=5,random_state=66),
+                  GradientBoostingClassifier(random_state=66),XGBClassifier(random_state=66)]   # objective='multi:softprob' objective='binary:logistic',
+reg_model_list = [DecisionTreeRegressor(max_depth=5,random_state=66),RandomForestRegressor(max_depth=5,random_state=66),
+                  GradientBoostingRegressor(random_state=66),XGBRegressor(random_state=66)]
 
 #3. 컴파일 훈련
 
+for name,data in dd.items():
+    
+    if name == 'Bike':
+        x = Bikedata.drop(['casual','registered','count'], axis=1)  
+        x['datetime'] = pd.to_datetime(x['datetime'])
+        x['year'] = x['datetime'].dt.year
+        x['month'] = x['datetime'].dt.month
+        x['day'] = x['datetime'].dt.day
+        x['hour'] = x['datetime'].dt.hour
+        x = x.drop('datetime', axis=1)
+        y = Bikedata['count']  # np.unique(y, return_counts = True 누가봐도 회귀모델
+        y = np.log1p(y)
+    else:
+        datasets = data
+        x = datasets.data
+        y = datasets.target
+        #print(datasets.feature_names) # feature의 이름들 확인 가능.
+        
+    #x = np.delete(x,0,axis=1)  # 이부분에서 feature 제거 가능
+    x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=66)
+    choice = np.unique(y, return_counts=True)[1].min()    
+    
+    print(f'{name} 데이터셋의 결과를 소개합니다~')
+
+    if choice > 4:       
+        print('나는 분류모델!')                                             
+        
+        plt.figure(figsize=(20,20))
+        for i,model in enumerate(cla_model_list,start=1):
+            model.fit(x_train,y_train)
+            print(f'{model}.score : ', model.score(x_test,y_test))   
+            print(model.feature_importances_)
+            plt.subplot(2,2,i)
+            plot_feature_importances_dataset(model)
+        plt.show()
+    
+        
+        print('\n')
+        
+    else:                    
+        print('나는 회귀모델!')  
+                                                   
+        plt.figure(figsize=(20,20))
+        for i,model in enumerate(reg_model_list,start=1):
+            model.fit(x_train,y_train)
+            print(f'{model}.score : ', model.score(x_test,y_test))   
+            print(model.feature_importances_)
+            plt.subplot(2,2,i)
+            plot_feature_importances_dataset(model)
+        plt.show()
+        
+        print('\n')
+
+                                
+
+
+           
+# min_index_val = np.min(feature_importances_)
+# min_index = np.where(feature_importances_ == min_index_val)[0][0]
+# print("min_index_val ",min_index_val)
+# print("min_index ",min_index)
 
 
 
 
-
-
-x = datasets.data  
-y = datasets.target
-#print(datasets.feature_names) 
-#print(datasets.DESCR)  
-#x = np.delete(x,0,axis=1)
-x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=66)
-
-#2. 모델링 모델구성
-
-
-
-
-#3. 컴파일,훈련
-
-                                    
-
-
-
-
-#1.내가 젤 처음 한 방식. model1~4를 for문으로 불러오지 못했다. model.fit도 4번 따로해줘야한다.
-'''
-plt.figure(figsize=(20,20))
-plt.subplot(2,2,1)
-plot_feature_importances_dataset(model1)
-plt.subplot(2,2,2)
-plot_feature_importances_dataset(model2)
-plt.subplot(2,2,3)
-plot_feature_importances_dataset(model3)
-plt.subplot(2,2,4)
-plot_feature_importances_dataset(model4)
-plt.show()
-'''                     
-
-
-#2. model+i index번호 매기는건 변수 활성화가 안먹히고 model_list에 model들을 담아서 불러온다.
-
-model_list = [model1,model2,model3,model4]
-plt.figure(figsize=(20,20))
-for i,model in enumerate(model_list,start=1):
-    model.fit(x_train,y_train)
-    plt.subplot(2,2,i)
-    plot_feature_importances_dataset(model)
-plt.show()
-
-# result = model.score(x_test,y_test)
-# y_pred = model.predict(x_test)
-# acc = accuracy_score(y_test,y_pred)
-# print("model.score : ",result)
-# print("accuracy_scroe : ",acc)        이 부분은 그냥 확인용. 
---
+++ ML/ML10~13_pipeline&feature_importance.py/m14_FI_0_masterpackage.py
@@ -0,0 +1,4 @@
+# 피처임포턴스가 전체 중요도에서 하위 20~25% 컬럼들을 제거하여 
+# 데이터셋 재구성 후 각 모델별로 돌려서 결과 도출!
+# 기존 모델결과와의 비교
+# DecisionTree,RandomForest,GradientBoosting,XGB 모델 4개 사용.
