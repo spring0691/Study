fe6e34fb05498b58222741c3713310421bf0c4e7
yeram <ye069182@gmail.com>
2022-01-27 14:32:15 +0900

    01/27

--- ML/ML28~29_Study/m30_smote3_wine_labelreduce.py
+++ ML/ML28~29_Study/m30_smote3_wine_labelreduce.py
@@ -1,8 +1,9 @@
 # 그냥 증폭해서 성능바교
 
+from tabnanny import verbose
 import numpy as np, pandas as pd, warnings
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split,RandomizedSearchCV
+from sklearn.model_selection import train_test_split,RandomizedSearchCV         #,cross_val_score,StratifiedKFold
 from sklearn.preprocessing import StandardScaler,MinMaxScaler,LabelEncoder
 from sklearn.metrics import f1_score,accuracy_score
 from sklearn.feature_selection  import SelectFromModel
@@ -22,19 +23,22 @@ y = np.where(y<=5,'Good',np.where(y==6,'Normal',np.where(y<=9,'Bad',y)))
 
 le = LabelEncoder()
 y = le.fit_transform(y)
-
 # print(np.unique(y,return_counts=True)) [0, 1, 2] [1060, 1640, 2198]
+
 
 x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=66,stratify=y)  
 # print(np.unique(y_train,return_counts=True)) #[0, 1, 2]    [ 848, 1312, 1758]
+
 
 # train데이터만 증폭시켜보자
 smote = SMOTE(random_state=66,k_neighbors=2)    # n_neighbors = k_neighbors + 1 인접한값을 참조하여 증폭하기때문에 최대값이 정해져있다.
 x_train,y_train = smote.fit_resample(x_train,y_train) 
 # print(np.unique(y_train,return_counts=True))  #[0, 1, 2] [1758, 1758, 1758]
 
+
 x_train,x_val,y_train,y_val = train_test_split(x_train,y_train, train_size=0.8, shuffle=True, random_state=66,stratify=y_train)  
 # print(np.unique(y_train,return_counts=True))  #[0, 1, 2] [1406, 1407, 1406]
+
 
 model = XGBClassifier(
     n_estimators = 1000,
@@ -47,12 +51,12 @@ model = XGBClassifier(
     eval_metric='merror',
     use_label_encoder=False,
 )
-
 model.fit(x_train,y_train,verbose=True,early_stopping_rounds=100,eval_set=[(x_val,y_val)])
 
+# kfold = StratifiedKFold(n_splits=5,shuffle=True,random_state=66)
+# cross_val_score(model,x_train,y_train,cv=kfold,verbose=2,early_stopping_rounds=100)
 
-
-print(f"md.score : {model.score(x_test,y_test)}")                               #0.689795918367347
+print(f"md.score : {model.score(x_test,y_test)}")                             #0.689795918367347
 print(f"ac_score : {accuracy_score(y_test,model.predict(x_test))}")             #0.689795918367347
 print(f"f1_score : {f1_score(y_test,model.predict(x_test),average='macro')}")   #0.6935184759783691
 print(f"f1_score : {f1_score(y_test,model.predict(x_test),average='micro')}")   #0.689795918367347
