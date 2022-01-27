d2abe316745be6abe6e798f52915bd8cb7c0724f
yeram <ye069182@gmail.com>
2022-01-27 14:16:10 +0900

    01/27

--- ML/ML28~29_Study/m30_smote3_wine_labelreduce.py
+++ ML/ML28~29_Study/m30_smote3_wine_labelreduce.py
@@ -18,21 +18,23 @@ datasets = pd.read_csv(path + 'winequality-white.csv',sep=';', index_col=None, h
 x = datasets.drop(['quality'],axis=1)   # (4898, 11)    
 y = datasets['quality']  
 
+y = np.where(y<=5,'Good',np.where(y==6,'Normal',np.where(y<=9,'Bad',y)))
+
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-
+# print(np.unique(y,return_counts=True)) [0, 1, 2] [1060, 1640, 2198]
 
 x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=66,stratify=y)  
-# print(np.unique(y_train,return_counts=True)) [0, 1, 2, 3, 4, 5, 6]    [  16,  130, 1166, 1758,  704,  140,   4]
+# print(np.unique(y_train,return_counts=True)) #[0, 1, 2]    [ 848, 1312, 1758]
 
 # train데이터만 증폭시켜보자
 smote = SMOTE(random_state=66,k_neighbors=2)    # n_neighbors = k_neighbors + 1 인접한값을 참조하여 증폭하기때문에 최대값이 정해져있다.
 x_train,y_train = smote.fit_resample(x_train,y_train) 
-# print(np.unique(y_train,return_counts=True))  [0, 1, 2, 3, 4, 5, 6] [1758, 1758, 1758, 1758, 1758, 1758, 1758]
+# print(np.unique(y_train,return_counts=True))  #[0, 1, 2] [1758, 1758, 1758]
 
 x_train,x_val,y_train,y_val = train_test_split(x_train,y_train, train_size=0.8, shuffle=True, random_state=66,stratify=y_train)  
-# print(np.unique(y_train,return_counts=True))  [0, 1, 2, 3, 4, 5, 6] [1407, 1406, 1406, 1406, 1406, 1407, 1406]
+# print(np.unique(y_train,return_counts=True))  #[0, 1, 2] [1406, 1407, 1406]
 
 model = XGBClassifier(
     n_estimators = 1000,
@@ -48,8 +50,9 @@ model = XGBClassifier(
 
 model.fit(x_train,y_train,verbose=True,early_stopping_rounds=100,eval_set=[(x_val,y_val)])
 
-print(f"md.score : {model.score(x_test,y_test)}")                               #0.6255102040816326
-print(f"ac_score : {accuracy_score(y_test,model.predict(x_test))}")             #0.6255102040816326
-print(f"f1_score : {f1_score(y_test,model.predict(x_test),average='macro')}")   #0.39340099432035197
-print(f"f1_score : {f1_score(y_test,model.predict(x_test),average='micro')}")   #0.6255102040816326
 
+
+print(f"md.score : {model.score(x_test,y_test)}")                               #0.689795918367347
+print(f"ac_score : {accuracy_score(y_test,model.predict(x_test))}")             #0.689795918367347
+print(f"f1_score : {f1_score(y_test,model.predict(x_test),average='macro')}")   #0.6935184759783691
+print(f"f1_score : {f1_score(y_test,model.predict(x_test),average='micro')}")   #0.689795918367347
