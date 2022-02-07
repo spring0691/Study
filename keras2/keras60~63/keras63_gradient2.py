862d38024bd7a77536ce5cd0f967581606e053b2
yeram <ye069182@gmail.com>
2022-02-07 18:56:27 +0900

    02/07

--- keras2/keras60~63/keras63_gradient2.py
+++ keras2/keras60~63/keras63_gradient2.py
@@ -16,14 +16,15 @@ def gradient2(x):
 
 # 미분 -> 2차 3차함수에서 각 지점에 대한 기울기.
 
-x = 10.0             # 초기값 
-epochs = 20
+# 초기세팅값
+x = -10           
+epochs = 25
 learning_rate = 0.25
 
-print("step\t x\t f(x)")
-print("{:02d}\t {:6.5f}\t {:6.5f}}\t".format(0,x,f(x)))
+print("step\t x\t\t f(x)")
+print("{:02d}\t {:6.5f}\t {:6.5f}\t".format(0,x,f(x)))      # 초기값 출력
 
 for i in range(epochs):
     x = x - learning_rate * gradient(x)
     
-    print("{:02d}\t {:6.5f}\t {:6.5f}\t".format(i+1,x,f(x)))
+    print("{:02d}\t {:6.5f}\t {:1.5f}\t".format(i+1,x,f(x)))
