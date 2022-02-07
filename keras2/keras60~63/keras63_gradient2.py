262dbd2a47c849533e17e7c0b9af56dd3bc1ddfd
yeram <ye069182@gmail.com>
2022-02-07 17:34:19 +0900

    02/07

--- keras2/keras60~63/keras63_gradient2.py
+++ keras2/keras60~63/keras63_gradient2.py
@@ -6,7 +6,7 @@ def f2(x):
     temp = x**2 - 4*x + 6
     return temp
 
-gradient = lambda x: 2*x -4
+gradient = lambda x: 2*x -4     # gradient는 f에 대한 기울기.weight.이다
 
 def gradient2(x):
     temp = 2*x-4
@@ -16,4 +16,14 @@ def gradient2(x):
 
 # 미분 -> 2차 3차함수에서 각 지점에 대한 기울기.
 
-x = 0.0 # 초기값 
+x = 10.0             # 초기값 
+epochs = 20
+learning_rate = 0.25
+
+print("step\t x\t f(x)")
+print("{:02d}\t {:6.5f}\t {:6.5f}}\t".format(0,x,f(x)))
+
+for i in range(epochs):
+    x = x - learning_rate * gradient(x)
+    
+    print("{:02d}\t {:6.5f}\t {:6.5f}\t".format(i+1,x,f(x)))
