0ea94ba8c00b29ed6bfe6cba07387e776d357cf8
yeram <ye069182@gmail.com>
2022-02-07 13:44:18 +0900

    02/07

--- keras2/keras60_ReduceLR_1_cifar100.py
+++ keras2/keras60_ReduceLR_1_cifar100.py
@@ -31,21 +31,22 @@ model.add(GlobalAveragePooling2D())     # Global은 10개의 평균값을 넘겨
 model.add(Dense(100, activation='softmax'))
 
 #3. 컴파일, 훈련
-learning_rate = 0.1
+learning_rate = 0.001           # 초기 lr이 되게 중요하다 
 optimizer = Adam(learning_rate=learning_rate)
 model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer,metrics=['acc'])
 
 es = EarlyStopping(monitor="val_loss", patience=15, mode='min',verbose=1, restore_best_weights=True)
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=5, mode='auto',verbose=1, factor=0.5)   
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=5, mode='auto',verbose=1,min_lr=0.0001,factor=0.5)   
 # 값이 갱신이 안되는순간 LR을 0.5배만큼 ( * 0.5) 감소시키겠다 
 # 모델이 돌아가다가 값이 5번 이내에 갱신이안되면 lr감소. 그리고 다시 5번 보다가 안되면 lr감소. 그래도 안되면 early스탑걸려서 멈춰지기때문에
 # 둘 사이의 값 조정이 필요
 
 start = time.time()
-model.fit(x_train,y_train,epochs=1000, batch_size=32,validation_split=0.2, callbacks=[reduce_lr,es])
+model.fit(x_train,y_train,epochs=1000, batch_size=64,validation_split=0.2, callbacks=[reduce_lr,es])
 end = time.time() - start
 
 loss, acc = model.evaluate(x_test,y_test,batch_size=32)
 
 print(f"lr : {learning_rate}, loss : {round(loss,4)}, acc : {round(acc,4)}, 걸린시간 : {round(end,4)}초")
 
+# lr : 0.001, loss : 2.2437, acc : 0.4225, 걸린시간 : 935.174초
