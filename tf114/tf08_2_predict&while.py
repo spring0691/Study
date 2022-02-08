2bbcb1e05a3b153b186757d3da4cb9ea18efc325
yeram <ye069182@gmail.com>
2022-02-08 18:43:34 +0900

    02/08

--- tf114/tf08_2_predict&while.py
+++ tf114/tf08_2_predict&while.py
@@ -12,55 +12,48 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.compat.v1.set_random_seed(77)
 
 #1. 데이터
-# x_train = [1,2,3]               # batch가 3인 상태와 같다.
-# y_train = [1,2,3]
+
 x_train = tf.compat.v1.placeholder(tf.float32, shape=[None])
 y_train = tf.compat.v1.placeholder(tf.float32, shape=[None])
 x_test = tf.compat.v1.placeholder(tf.float32, shape=[None])
 
-# w = tf.compat.v1.Variable(1, dtype=tf.float32)
-# b = tf.compat.v1.Variable(1, dtype=tf.float32)
 w = tf.compat.v1.Variable(tf.random_normal([1]), dtype=tf.float32)
 b = tf.compat.v1.Variable(tf.random_normal([1]), dtype=tf.float32)
 
-# sess = tf.compat.v1.Session()
-# sess.run(tf.global_variables_initializer())
-# print(sess.run(w))  #이 한줄을 출력하기위해 위의 옵션이 필요하다...
 
 #2. 모델구성
 hypothesis = x_train * w + b           # hypothesis(가설) = y_predict
 
 #3-1. 컴파일
 loss = tf.reduce_mean(tf.square(hypothesis - y_train))  
-
 optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.1)   
-
-train = optimizer.minimize(loss)    
+train = optimizer.minimize(loss) 
 
 
 #3-2. 훈련
-# test_list = [4]
 
-with tf.compat.v1.Session() as sess:        # tf.~~~Session()을 sess로써 실행하고 작업이 다 끝나면 종료해라.
-# sess.close()    # session은 항상 열었으면 닫아주어야한다. with문을 쓰면 자동으로 종료된다.
-# sess = tf.compat.v1.Session()
+with tf.compat.v1.Session() as sess:        
+    
     sess.run(tf.compat.v1.global_variables_initializer())
-
     step = 0
+    
     while True:
+        
         step += 1
         # sess.run(train)     # 여기서 실행이 일어난다.
         _, loss_val, w_val, b_val = sess.run([train, loss, w, b], feed_dict={x_train:[1,2,3], y_train:[1,2,3]})
         
         if step % 20 == 0:
-            # print(f"{step+1}, {sess.run(loss)}, {sess.run(w)}, {sess.run(b)}")
-            print(step, loss_val, w_val, b_val)
-        
-        if w_val >= 0.9999999:
+
+            print(step, loss_val, w_val, b_val) 
+                  
+        if loss_val <= 1e-8:                    # if w_val >= 0.999:
             
             predict = x_test*w+b
             
-            predict = sess.run(predict,feed_dict={x_test:[6,7,8]})
-            print(predict)
+            predict1 = sess.run(predict,feed_dict={x_test:[4]})
+            predict2 = sess.run(predict,feed_dict={x_test:[5,6]})
+            predict3 = sess.run(predict,feed_dict={x_test:[7,8,9]})
+            print(f"{predict1}\n{predict2}\n{predict3}")
             break
         
