ff32d8a2d72bc642d3ba315c683021cd9d9fd7b7
yeram <ye069182@gmail.com>
2022-02-10 16:01:31 +0900

    02/10

--- tf114/tf09~15/tf15_softmax.py
+++ tf114/tf09~15/tf15_softmax.py
@@ -1,4 +1,5 @@
 import numpy as np, tensorflow as tf,os
+from sklearn.metrics import accuracy_score
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.compat.v1.set_random_seed(66)
 
@@ -18,3 +19,45 @@ y_data = [[0,0,1],      # 2
           [0,1,0],
           [1,0,0],      # 0
           [1,0,0]]
+
+x_predict = [[1,11,7,9]] # (1,4) -> (N,4)
+#2. 모델구성
+x = tf.compat.v1.placeholder(tf.float32, shape=[None,4])
+y = tf.compat.v1.placeholder(tf.float32, shape=[None,3])
+
+w = tf.compat.v1.Variable(tf.compat.v1.random_normal([4,3]),name='weight')
+b = tf.compat.v1.Variable(tf.compat.v1.random_normal([1,3]),name='bias')        # y의 colmun이 3개이기 때문.
+
+hypothesis = tf.nn.softmax(tf.matmul(x,w) + b)
+# model.add(Dense(3, activation='softmax))
+
+#3-1.컴파일
+# loss = -tf.reduce_mean(y*tf.math.log(hypothesis)+(1-y)*tf.math.log(1-hypothesis))     # binary nono~
+loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(hypothesis), axis=1))
+# categorical_crossentropy
+
+# optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.005).minimize(loss)      
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.6).minimize(loss)       
+
+#3-2. 훈련
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+
+step = 0 
+
+while True:
+    step += 1
+    p =1000
+    
+    loss_val,_ = sess.run([loss,optimizer],feed_dict={x:x_data,y:y_data})
+    print(f"{step:05d} \t{loss_val}")
+    
+    
+    if loss_val < 0.0047:
+        #4. 평가, 예측
+        
+        results = sess.run(hypothesis, feed_dict={x:x_predict})
+        print(results, sess.run(tf.math.argmax(results,1)))
+        break
+        
+sess.close()
