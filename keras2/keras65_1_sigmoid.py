643b43fcd82dbd53bbd5a849b70cc86aa27357dd
yeram <ye069182@gmail.com>
2022-02-10 10:28:22 +0900

    02/10

--- keras2/keras65_1_sigmoid.py
+++ keras2/keras65_1_sigmoid.py
@@ -6,7 +6,6 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))     #  np.exp(-x) -> 1 / (1 + e^-x)
 
 x = np.arange(-5, 5, 0.1)
-print(len(x))
 
 y = sigmoid(x)      # 어떤 x값을 넣더라도 결과값이 0과 1사이로 수렴한다.
 
--- keras2/keras65_4_softmax.py
+++ keras2/keras65_4_softmax.py
@@ -1,2 +1,11 @@
 import numpy as np, matplotlib.pyplot as plt
 
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x))
+
+x = np.arange(-5, 5, 0.1)
+y = softmax(x)      
+
+plt.plot(x,y)
+plt.grid()
+plt.show()
