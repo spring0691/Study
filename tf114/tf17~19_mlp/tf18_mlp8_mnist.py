9876e681cb40658d0afe6c59c45cb760b2dd54cc
yeram <ye069182@gmail.com>
2022-02-14 11:06:38 +0900

    02/14

--- tf114/tf17~19_mlp/tf18_mlp8_mnist.py
+++ tf114/tf17~19_mlp/tf18_mlp8_mnist.py
@@ -1,66 +1,53 @@
 from keras.datasets import mnist
 import tensorflow as tf, numpy as np,os
 from sklearn.metrics import accuracy_score
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.compat.v1.set_random_seed(66)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(len(x_train),-1)      # 60000, 784
-x_test = x_test.reshape(len(x_test),-1)         # 10000, 784
+x_train = x_train.reshape(len(x_train),-1)/255.      # 60000, 784
+x_test = x_test.reshape(len(x_test),-1)/255.         # 10000, 784
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
 #2. 모델
-x = tf.compat.v1.placeholder(tf.float32,shape=[None,784])    
-y = tf.compat.v1.placeholder(tf.float32,shape=[None,1]) 
+x = tf.compat.v1.placeholder(tf.float32, shape=[None, x_train.shape[1]])
+y = tf.compat.v1.placeholder(tf.float32, shape=[None, 10])
 
-w = tf.compat.v1.Variable(tf.zeros([784,1024]),'weights')
-b = tf.compat.v1.Variable(tf.zeros([1,1024]))
+w = tf.compat.v1.Variable(tf.random.normal([x_train.shape[1], 64], mean=0.0, stddev=tf.math.sqrt(2/(13+64)), name='weight'))
+b = tf.compat.v1.Variable(tf.zeros([64]), name='bias')
 
 hidden_layer1 = tf.matmul(x, w) + b
 
-w1 = tf.compat.v1.Variable(tf.random.normal([1024,512], name='weight1'))
-b1 = tf.compat.v1.Variable(tf.random.normal([1,512], name='bias1'))
+w1 = tf.compat.v1.Variable(tf.random.normal([64, 32], mean=0, stddev=tf.math.sqrt(2/(64+32)), name='weight1'))
+b1 = tf.compat.v1.Variable(tf.zeros([32], name='bias1'))
 
 hidden_layer2 = tf.nn.relu(tf.matmul(hidden_layer1, w1) + b1)
 
-w2 = tf.compat.v1.Variable(tf.random.normal([512, 256], name='weight2'))
-b2 = tf.compat.v1.Variable(tf.random.normal([1,256], name='bias2'))
+w2 = tf.compat.v1.Variable(tf.random.normal([32, 16], mean=0, stddev=tf.math.sqrt(2/(32+16)), name='weight2'))
+b2 = tf.compat.v1.Variable(tf.zeros([16], name='bias2'))
 
-hidden_layer3 = tf.matmul(hidden_layer2, w2) + b2
+hidden_layer3 = tf.nn.relu(tf.matmul(hidden_layer2, w2) + b2)
 
-w3 = tf.compat.v1.Variable(tf.random.normal([256, 128], name='weight3'))
-b3 = tf.compat.v1.Variable(tf.random.normal([1,128], name='bias3'))
+w3 = tf.compat.v1.Variable(tf.random.normal([16, 4], mean=0, stddev=tf.math.sqrt(2/(16+4)), name='weight3'))
+b3 = tf.compat.v1.Variable(tf.zeros([4], name='bias3'))
 
 hidden_layer4 = tf.nn.relu(tf.matmul(hidden_layer3, w3) + b3)
 
-w4 = tf.compat.v1.Variable(tf.random.normal([128, 64], name='weight4'))
-b4 = tf.compat.v1.Variable(tf.random.normal([1,64], name='bias4'))
+w4 = tf.compat.v1.Variable(tf.random.normal([4, 10], mean=0, stddev=tf.math.sqrt(2/(4+1)), name='weight3'))
+b4 = tf.compat.v1.Variable(tf.zeros([10], name='bias3'))
 
-hidden_layer5 = tf.nn.relu(tf.matmul(hidden_layer4, w4) + b4)
-
-w5 = tf.compat.v1.Variable(tf.random.normal([64, 32], name='weight5'))
-b5 = tf.compat.v1.Variable(tf.random.normal([1,32], name='bias5'))
-
-hidden_layer6 = tf.nn.relu(tf.matmul(hidden_layer5, w5) + b5)
-
-w5 = tf.compat.v1.Variable(tf.random.normal([32, 16], name='weight5'))
-b5 = tf.compat.v1.Variable(tf.random.normal([1,16], name='bias5'))
-
-hidden_layer7 = tf.nn.relu(tf.matmul(hidden_layer6, w5) + b5)
-
-w6 = tf.compat.v1.Variable(tf.random.normal([16, 10], name='weight6'))
-b6 = tf.compat.v1.Variable(tf.random.normal([1,10], name='bias6'))
-
-hypothesis = tf.nn.softmax(tf.matmul(hidden_layer7,w6) + b6)
+hypothesis = tf.nn.softmax(tf.matmul(hidden_layer4,w4) + b4)
        
 #3-1. 컴파일
-loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(hypothesis), axis=1))   # categorical_crossentropy
+loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(hypothesis), axis=1))   # categorical_crossentropy
 
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.0000005)       
-# optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.000000005)      
-
-train = optimizer.minimize(loss)
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.0005).minimize(loss)      
+# optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.000000000005)   .minimize(loss)   
 
 #3-2. 훈련
 sess = tf.compat.v1.Session()
@@ -72,7 +59,7 @@ while True:
     step += 1
     p =1000
     
-    loss_val,_ = sess.run([loss,train],feed_dict={x:x_train,y:y_train})
+    loss_val,_ = sess.run([loss,optimizer],feed_dict={x:x_train,y:y_train})
     print(f"{step:05d} \t{loss_val:.7f}")
     
     if loss_val < 0.105:       
--- tf20_cnn2.py
+++ tf20_cnn2.py
@@ -1,3 +1,24 @@
 import numpy as np, tensorflow as tf
 tf.compat.v1.set_random_seed(66)
 
+#1. 데이터
+image = np.array([[[[1], [2], [3]],
+                    [[4], [5], [6]],
+                    [[7], [8], [9]]]])
+
+print(image.shape)
+
+x = tf.compat.v1.placeholder(tf.float32, [None, 3, 3, 1])
+w = tf.constant([[[[1.]], [[1.]]],
+                  [[[1.]], [[1.]]]])
+
+print(w)  # Tensor("Const:0", shape=(2, 2, 1, 1), dtype=float32)
+
+L1 = tf.nn.conv2d(x, w, strides=(1, 1, 1, 1), padding='VALID')
+
+print(L1)  # Tensor("Conv2D:0", shape=(?, 2, 2, 1), dtype=float32)
+
+sess = tf.compat.v1.Session()
+output = sess.run(L1, feed_dict={x:image})  # 그냥 곱해준다 행렬곱 x 
+
+print(output, "\n", output.shape)
