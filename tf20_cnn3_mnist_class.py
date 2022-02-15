f9af5f4fdce2757407181b0a49377abaf494a6b7
yeram <ye069182@gmail.com>
2022-02-15 22:39:13 +0900

    02/15

--- tf20_cnn3_mnist_class.py
+++ tf20_cnn3_mnist_class.py
@@ -22,7 +22,7 @@ y = tf.compat.v1.placeholder(tf.float32, [None,10])
 #2. 모델구성
 
 # Layer1 
-w1 = tf.compat.v1.get_variable('w1', shape = [2, 2, 1, 16])    
+w1 = tf.compat.v1.get_variable('w1', shape = [2, 2, 1, 128])    
 L1 = tf.nn.conv2d(x, w1, strides=[1,1,1,1], padding='SAME')    
 L1 = tf.nn.relu(L1)   # activation 적용
 L1_maxpool = tf.nn.max_pool2d(L1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')    # 2,2는 maxpool값.   max_pool default = VALID
@@ -34,7 +34,7 @@ L1_maxpool = tf.nn.max_pool2d(L1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='S
 # print(L1_maxpool)   # Tensor("MaxPool:0", shape=(?, 14, 14, 64), dtype=float32)
 
 # Layers2 
-w2 = tf.compat.v1.get_variable('w2', shape=[3, 3, 16, 8])
+w2 = tf.compat.v1.get_variable('w2', shape=[3, 3, 128, 64])
 L2 = tf.nn.conv2d(L1_maxpool, w2, strides=[1,1,1,1], padding='SAME')    
 L2 = tf.nn.selu(L2)
 L2_maxpool = tf.nn.max_pool2d(L2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')    # 2,2는 maxpool값.   max_pool default = VALID
@@ -42,40 +42,47 @@ L2_maxpool = tf.nn.max_pool2d(L2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='S
 
 
 # Layers3 
-w3 = tf.compat.v1.get_variable('w3', shape=[3, 3, 8, 4])
+w3 = tf.compat.v1.get_variable('w3', shape=[3, 3, 64, 32])
 L3 = tf.nn.conv2d(L2_maxpool, w3, strides=[1,1,1,1], padding='SAME')    
 L3 = tf.nn.elu(L3)
 L3_maxpool = tf.nn.max_pool2d(L3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')    # 2,2는 maxpool값.   max_pool default = VALID
-# print(L3_maxpool)   # Tensor("MaxPool_2:0", shape=(?, 4, 4, 64), dtype=float32)
+# print(L3_maxpool)   # Tensor("MaxPool_2:0", shape=(?, 4, 4, 32), dtype=float32)
+
+# Layer4
+w4 = tf.compat.v1.get_variable('w4', shape=[3, 3, 32, 32], initializer=tf.contrib.layers.xavier_initializer())
+L4 = tf.nn.conv2d(L3_maxpool, w4, strides=[1, 1, 1, 1], padding='SAME')  
+L4 = tf.nn.elu(L4)
+L4_maxpool = tf.nn.max_pool2d(L4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+# print(L4_maxpool)   # Tensor("MaxPool_3:0", shape=(?, 2, 2, 32), dtype=float32)
 
 
 # Flatten 
-L_flat = tf.reshape(L3_maxpool,[-1,4*4*4])
+L_flat = tf.reshape(L3_maxpool,[-1,2*2*32])
 # print(f"플래튼 : {L_flat}")     # (?, 128)
 
 # Layer5    DNN
-w5 = tf.get_variable('w5',shape=[4*4*4, 64], initializer=tf.contrib.layers.xavier_initializer())
+w5 = tf.compat.v1.get_variable('w5',shape=[2*2*32, 64], initializer=tf.contrib.layers.xavier_initializer())
 b5 = tf.compat.v1.Variable(tf.random.normal([64]), name='b5')
-L4 =  tf.nn.selu(tf.matmul(L_flat,w5) + b5)
-L4 = tf.nn.dropout(L4, keep_prob=0.5)
+L5 =  tf.nn.selu(tf.matmul(L_flat,w5) + b5)
+L5 = tf.nn.dropout(L5, keep_prob=0.5)   # `rate = 1 - keep_prob`
 
 # Layer6    DNN
-w6 = tf.get_variable('w6',shape=[64, 32], initializer=tf.contrib.layers.xavier_initializer())
+w6 = tf.compat.v1.get_variable('w6',shape=[64, 32], initializer=tf.contrib.layers.xavier_initializer())
 b6 = tf.compat.v1.Variable(tf.random.normal([32]), name='b6')
-L5 =  tf.nn.relu(tf.matmul(L4,w6) + b6)
-L5 = tf.nn.dropout(L5, keep_prob=0.5)
+L6 =  tf.nn.relu(tf.matmul(L5,w6) + b6)
+L6 = tf.nn.dropout(L6, rate=0.5)
 
 # Layer7    DNN
-w7 = tf.get_variable('w6',shape=[32, 10], initializer=tf.contrib.layers.xavier_initializer())
+w7 = tf.compat.v1.get_variable('w7',shape=[32, 10], initializer=tf.contrib.layers.xavier_initializer())
 b7 = tf.compat.v1.Variable(tf.random.normal([10]), name='b6')
-L6 =  tf.nn.relu(tf.matmul(L5,w7) + b7)
-L6 = tf.nn.dropout(L6, keep_prob=0.5)
-hypothesis = tf.nn.softmax(L6)
+L7 =  tf.nn.relu(tf.matmul(L6,w7) + b7)
+L7 = tf.nn.dropout(L7, rate=0.5)
+hypothesis = tf.nn.softmax(L7)
 
 
 #3-1. 컴파일
 loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(hypothesis), axis=1))   # categorical_crossentropy
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.01).minimize(loss)      
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.001).minimize(loss)      
 # optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.000000005)   .minimize(loss)   
 
 
@@ -84,9 +91,40 @@ sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
 step = 0 
+training_epochs = 100
+batch_size = 100
+total_batch = int(len(x_train)/batch_size)  
+# 만약에 / 안맞아 떨어지면 모자란 값은 버린다.
+
+for epoch in range(training_epochs):
+    avg_loss = 0
+    
+    for i in range(total_batch):    # 몇번? 600번
+        
+        start = i * batch_size      # 0
+        end = start + batch_size    # 100
+        batch_x, batch_y = x_train[start:end], y_train[start:end] # 0~100
+        
+        feed_dict = {x:batch_x,y:batch_y}   # 단순변수
+        
+        batch_loss, _ = sess.run([loss, optimizer],feed_dict=feed_dict)
+
+        avg_loss += batch_loss / total_batch
+    
+    print(f"Epoch : {epoch+1:04d}, loss : {avg_loss:.9f}")
+
+print("훈련 끝")
+
+prediction = tf.equal(tf.math.argmax(hypothesis,1), tf.math.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+print(f'ACC : {sess.run(accuracy,feed_dict={x:x_test,y:y_test})}')
 
 
+
+
+'''
 while True:
+    avg_loss = 0
     step += 1
     p =1000
     
@@ -110,5 +148,5 @@ while True:
         print(f"train_acc : {train_acc} test_acc : {test_acc}")
        
         break
-    
+'''    
 sess.close()
