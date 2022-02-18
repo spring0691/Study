244fd8a9457bf66ab79c49c180b141c9180639c6
yeram <ye069182@gmail.com>
2022-02-18 14:20:44 +0900

    02/18

--- keras2/keras67_2_vgg16.py
+++ keras2/keras67_2_vgg16.py
@@ -7,10 +7,17 @@ from tensorflow.keras.datasets import mnist, cifar10, cifar100
 
 vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(32,32,3))
 
+# vgg16.summary()
+# vgg16.trainable = False     # 가중치를 동결시킨다!
+
 model = Sequential()
 model.add(vgg16)
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(10))
 
-model.summary()
+# model.trainable = False    각 레이어하나마다 이름을 주고 동결시킬수도 있다.(Funtional)에서 더 편하게 쓸 수 있다.
+
+# model.summary()
+# print(len(model.weights))               # 각 layers = weights + bias가 1세트 2개.
+# print(len(model.trainable_weights))     # layer가 1개 줄어들어서 len은 30이 나온다.
