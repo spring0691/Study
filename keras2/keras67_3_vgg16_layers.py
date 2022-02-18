5ea4c581b753b4cf0a66b2e4a5c9fa4cd7ca5689
yeram <ye069182@gmail.com>
2022-02-18 14:30:24 +0900

    02/18

---
+++ keras2/keras67_3_vgg16_layers.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.datasets import mnist, cifar10, cifar100
+
+(x_train,y_train), (x_test,y_test) = cifar10.load_data()
+
+vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(32,32,3))
+
+# vgg16.trainable = False     # 가중치를 동결시킨다!
+
+model = Sequential()
+model.add(vgg16)
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(10))
+
+model.trainable = False
+
+import pandas as pd
+pd.set_option('max_colwidth', -1)
+layers = [(layer, layer.name, layer.trainable) for layer in model.layers]
+results = pd.DataFrame(layers, columns = ['Layer Type','Layer Name','Layer Trainable'])
+print(results)
