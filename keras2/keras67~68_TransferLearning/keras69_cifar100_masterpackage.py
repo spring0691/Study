f108f16677596e47e0063a26a31324df4c575c0b
yeram <ye069182@gmail.com>
2022-02-21 11:12:29 +0900

    02/21

--- keras2/keras67~68_TransferLearning/keras69_cifar100_masterpackage.py
+++ keras2/keras67~68_TransferLearning/keras69_cifar100_masterpackage.py
@@ -1,3 +1,58 @@
 # VGG19, Xception, ResNet50, ResNet101, InceptionV3 ,InceptionResNetV2, DenseNet121, MobileNetV2, NasNetMobile, EfficeintNetB0,
 # fine tuning의 정의 : 전이학습에서 위와 아래 추가로 붙여주는 부분을 개발자가 튜닝하는것.
 # pre_trained model : 전이학습처럼 사전 훈련된 모델을 사용한 경우를 일컬음.
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,GlobalAveragePooling2D
+from tensorflow.keras.applications import VGG19, Xception, ResNet50, ResNet101, InceptionV3
+from tensorflow.keras.applications import InceptionResNetV2, DenseNet121, MobileNetV2, NASNetMobile, EfficientNetB0
+from tensorflow.keras.datasets import cifar100
+from keras.callbacks import EarlyStopping , ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+import numpy as np, time, warnings
+
+warnings.filterwarnings(action='ignore')
+
+(x_train,y_train), (x_test,y_test) = cifar100.load_data()
+
+# print(x_train.shape,x_test.shape)               # 32,32,3
+# print(len(np.unique(y_test)))                   # 100
+
+x_train = x_train.reshape(50000,32,32,3)/255.
+x_test = x_test.reshape(10000,32,32,3)/255.
+
+# aa = NasNetMobile(weights='imagenet')
+model_list = [VGG19(weights='imagenet', include_top=False, input_shape=(32,32,3)),
+            #   Xception(weights='imagenet', include_top=False, input_shape=(32,32,3)),             minsize = 75 
+              ResNet50(weights='imagenet', include_top=False, input_shape=(32,32,3)),
+              ResNet101(weights='imagenet', include_top=False, input_shape=(32,32,3)),
+            #   InceptionV3(weights=None, include_top=False, input_shape=(32,32,3)),          #minsize = 75
+            #   InceptionResNetV2(weights=None, include_top=False, input_shape=(32,32,3)),    #minsize = 75
+              DenseNet121(weights='imagenet', include_top=False, input_shape=(32,32,3)),
+              MobileNetV2(weights='imagenet', include_top=False, input_shape=(32,32,3)),
+              NASNetMobile(weights=None, include_top=False, input_shape=(32,32,3)),                 #weights= None하면 32,32,3 사용가능
+              EfficientNetB0(weights='imagenet', include_top=False, input_shape=(32,32,3))]
+
+for model in model_list:
+    print(f"모델명 : {model.name}")
+    TL_model = model
+    TL_model.trainable = True
+    model = Sequential()
+    model.add(TL_model)    
+    model.add(GlobalAveragePooling2D())  
+    model.add(Dense(1024))
+    model.add(Dense(512))
+    model.add(Dense(100,activation='softmax'))
+    
+    optimizer = Adam(learning_rate=0.0001)  # 1e-4     
+    lr=ReduceLROnPlateau(monitor= "val_acc", patience = 3, mode='max',factor = 0.1, min_lr=0.00001,verbose=1)
+    es = EarlyStopping(monitor ="val_acc", patience=10, mode='max',verbose=1,restore_best_weights=True)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics='acc')
+
+    model.fit(x_train,y_train,batch_size=200,epochs=1000,validation_split=0.2,callbacks=[lr,es], verbose=False)
+    
+    loss, Acc = model.evaluate(x_test,y_test,batch_size=100,verbose=False)
+    
+    print(f"loss : {round(loss,4)}")
+    print(f"Acc : {round(Acc,4)}")
+    
