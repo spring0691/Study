c0c45fd3a3b7564b1fc2b93445bd06907425ebd3
yeram <ye069182@gmail.com>
2022-02-21 10:14:20 +0900

    02/21

--- keras2/keras67~68_TransferLearning/keras69_cifar100_masterpackage.py
+++ keras2/keras67~68_TransferLearning/keras69_cifar100_masterpackage.py
@@ -1,3 +1,3 @@
-# VGG19, Xception, ResNet50, ResNet101, InceptionResNetV2, DenseNet121, MobileNetV2, 
+# VGG19, Xception, ResNet50, ResNet101, InceptionV3 ,InceptionResNetV2, DenseNet121, MobileNetV2, NasNetMobile, EfficeintNetB0,
 # fine tuning의 정의 : 전이학습에서 위와 아래 추가로 붙여주는 부분을 개발자가 튜닝하는것.
 # pre_trained model : 전이학습처럼 사전 훈련된 모델을 사용한 경우를 일컬음.
