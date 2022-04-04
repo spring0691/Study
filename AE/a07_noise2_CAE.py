64a8dbfbd98d1fb5758be7eab7ad37d21d344e88
yeram <ye069182@gmail.com>
2022-04-04 15:33:57 +0900

    04/04

--- AE/a07_noise2_CAE.py
+++ AE/a07_noise2_CAE.py
@@ -1,4 +1,6 @@
-# 2번 카피해서 복붙, CNN으로 딥하게 구성
+# 과제
+# mnist에 잡음추가하고 오토인코더 Conv2d사용하여 만들고 비교
+
 # Conv2D, MaxPool, Conv2D, MaxPool, Conv2D                                  -> Encoder
 # Conv2D, UpSampling2D, Conv2D, UpSampling2D, Conv2D, UpSampling2D, Conv2D  -> Decoder
 
@@ -43,12 +45,6 @@ def show_img(original,decoded_img,n):
         
     plt.show()
 
-# def autoencoder(hidden_layer_size):
-#     model = Sequential([
-#         Dense( units=hidden_layer_size, input_shape= ( x_train.shape(1), ), activation='relu'),
-#         Dense( units=x_train.shape(1), activation='sigmoid') 
-#     ])
-#     return model
 
 #1. 데이터
 (x_train, _) , (x_test, _)  = mnist.load_data()
@@ -56,24 +52,28 @@ def show_img(original,decoded_img,n):
 x_train = x_train.reshape(60000,28,28,1).astype('float')/255   # 또는 /255. 으로 나눠도 된다.
 x_test = x_test.reshape(10000,28,28,1).astype('float')/255   
 
+x_train_noised = x_train + np.random.normal(0, 0.1, size = x_train.shape)   
+x_test_noised = x_test + np.random.normal(0, 0.1, size = x_test.shape)
+x_train_noised = np.clip(x_train_noised, a_min=0, a_max=1)                
+x_test_noised = np.clip(x_test_noised, a_min=0, a_max=1) 
+
 #2. 모델
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Dense, Input, Conv2D, MaxPooling2D, UpSampling2D
 
-model = autoencoder(hidden_layer_size=32)
+model = autoencoder(hidden_layer_size=64)
 
-#3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam')
-model.fit(x_train,x_train,epochs=10)
+#3. 컴파일,훈련
+model.compile(optimizer='adam', loss='binary_crossentropy')
+model.fit(x_train_noised,x_train, epochs=10)
 
-#4. 평가, 예측
-output = model.predict(x_test)
+output = model.predict(x_test_noised)
 
 from matplotlib import pyplot as plt
 import random
 
-fig, ((ax1, ax2, ax3, ax4, ax5), (ax6, ax7, ax8, ax9, ax10)) = \
-    plt.subplots(2,5,figsize = (20,7))      # \는 줄바꿈 위해
+fig, ((ax1, ax2, ax3, ax4, ax5), (ax11, ax12, ax13, ax14, ax15),(ax6, ax7, ax8, ax9, ax10)) = \
+    plt.subplots(3,5,figsize = (20,7))      # \는 줄바꿈 위해
 
 # 이미지 5개를 무작위로 고른다.
 random_images = random.sample(range(output.shape[0]), 5)
@@ -83,6 +83,15 @@ for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
     ax.imshow(x_test[random_images[i]].reshape(28,28), cmap = 'gray')
     if i == 0:
         ax.set_ylabel("INPUT", size = 20)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+# 잡음을 넣은 이미지
+for i, ax in enumerate([ax11, ax12, ax13, ax14, ax15]):
+    ax.imshow(x_test_noised[random_images[i]].reshape(28,28), cmap = 'gray')
+    if i == 0:
+        ax.set_ylabel("NOISED", size = 20)
     ax.grid(False)
     ax.set_xticks([])
     ax.set_yticks([])
