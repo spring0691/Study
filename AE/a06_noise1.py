51f580bcd58094394318a91ded32e9722c545429
yeram <ye069182@gmail.com>
2022-04-04 10:40:05 +0900

    04/04

---
+++ AE/a06_noise1.py
@@ -0,0 +1,69 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+
+#1. 데이터
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784).astype('float')/255
+x_test = x_test.reshape(10000, 784).astype('float')/255
+
+x_train_noised = x_train + np.random.normal(0, 0.1, size = x_train.shape)   # 모든 각 픽셀에 0~0.1사이의 값을 랜덤하게 더해줌 -> 잡음이 생기는 효과
+x_test_noised = x_test + np.random.normal(0, 0.1, size = x_test.shape)
+x_train_noised = np.clip(x_train_noised, a_min=0, a_max=1)          # min이하값은 a_min값으로 max이상값은 a_max값으로 특정시켜줌.      
+x_test_noised = np.clip(x_test_noised, a_min=0, a_max=1)      
+
+#2. 모델
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+def autoencoder(hidden_layer_size):
+    model = Sequential()
+    model.add(Dense(units=hidden_layer_size,input_shape=(784,),activation='relu'))
+    model.add(Dense(units=784,activation='sigmoid'))
+    return model
+    
+model = autoencoder(hidden_layer_size=154)      # pca기준으로 손실률 0.95이상 -> 154
+
+#3. 컴파일,훈련
+model.compile(optimizer='adam', loss='binary_crossentropy')
+model.fit(x_train_noised,x_train, epochs=10)
+
+output = model.predict(x_test_noised)
+
+from matplotlib import pyplot as plt
+import random
+
+fig, ((ax1, ax2, ax3, ax4, ax5), (ax11, ax12, ax13, ax14, ax15),(ax6, ax7, ax8, ax9, ax10)) = \
+    plt.subplots(3,5,figsize = (20,7))      # \는 줄바꿈 위해
+
+# 이미지 5개를 무작위로 고른다.
+random_images = random.sample(range(output.shape[0]), 5)
+
+# 원본(입력) 이미지를 맨 위에 그린다.
+for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+    ax.imshow(x_test[random_images[i]].reshape(28,28), cmap = 'gray')
+    if i == 0:
+        ax.set_ylabel("INPUT", size = 20)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+# 잡음을 넣은 이미지
+for i, ax in enumerate([ax11, ax12, ax13, ax14, ax15]):
+    ax.imshow(x_test_noised[random_images[i]].reshape(28,28), cmap = 'gray')
+    if i == 0:
+        ax.set_ylabel("NOISED", size = 20)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+for i, ax in enumerate([ax6, ax7, ax8, ax9, ax10]):
+    ax.imshow(output[random_images[i]].reshape(28,28), cmap = 'gray')
+    if i == 0:
+        ax.set_ylabel("OUTPUT", size = 20)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+plt.tight_layout()
+plt.show()
