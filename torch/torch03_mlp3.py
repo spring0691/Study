883a0fef0988a7aed079aaced2e0f60fe803a35f
yeram <ye069182@gmail.com>
2022-05-11 17:54:26 +0900

    05/11

--- torch/torch03_mlp3.py
+++ torch/torch03_mlp3.py
@@ -9,15 +9,16 @@ USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
 
 #1. 데이터 정제해서 값 도출
-x = np.array( [[1,2,3,4,5,6,7,8,9,10],[1,1.1,1.2,1.3,1.4,1.5,1.6,1.5,1.4,1.3],[10,9,8,7,6,5,4,3,2,1]])   
-y = np.array([11,12,13,14,15,16,17,18,19,20])
+x = np.array([range(10), range(21,31), range(201,211)])
+y = np.array([[1,2,3,4,5,6,7,8,9,10],[1,1.1,1.2,1.3,1.4,1.5,1.6,1.5,1.4,1.3],[10,9,8,7,6,5,4,3,2,1]]) 
 
-x = np.transpose(x) # (3,10) -> (10,3)
+x = np.transpose(x) # (10, 3)
+y = np.transpose(y) # (10, 3)
 
 x = torch.FloatTensor(x).to(DEVICE)   
-y = torch.FloatTensor(y).unsqueeze(1).to(DEVICE)   # (10,) -> (10,1)
+y = torch.FloatTensor(y).to(DEVICE)  
 
-print(x.shape,y.shape)  # torch.Size([10, 3]) torch.Size([10, 1])
+# print(x.shape,y.shape)  # torch.Size([10, 3]) torch.Size([10, 1])
 
 #2. 모델구성
 model = nn.Sequential(
@@ -25,7 +26,7 @@ model = nn.Sequential(
     nn.Linear(5,3),
     nn.Linear(3,4),
     nn.Linear(4,2),
-    nn.Linear(2,1),
+    nn.Linear(2,3),
 ).to(DEVICE)    #Linear을 Sequential로 묶었다.
 
 #3. 컴파일, 훈련     
@@ -51,7 +52,7 @@ while True:
     loss = train(model,criterion,optimizer,x,y)    # 현재는 data loader 쓰지 않는 상태
     if epochs % 100 == 0 : print(f'epoch : {epochs}, loss : {loss}')
     # time.sleep(0.1)
-    if loss < 0.000001: break
+    if loss < 0.006: break
 print('==================================================================================')
 
 #4. 평가, 예측
@@ -68,9 +69,5 @@ def evaluate(model,criterion, x, y):
 loss2 = evaluate(model, criterion,x, y)
 print(f'최종 loss : {loss2}')
 
-result = model(torch.Tensor([[10,1.3,1]]).to(DEVICE))  
-print(f'[10,1.3,1]의 예측값 : {result.item()}')
-'''
-최종 loss : 9.850564310909249e-07
-[10,1.3,1]의 예측값 : 19.998193740844727
-'''
+result = model(torch.Tensor([[9,30,210]]).to(DEVICE))  
+print(f'[9,30,210]의 예측값 : {result}')
